56d37cbd0038130c92880505f196e96d8f355ec5
ChaunteVanDeBurgh <chauntevandeburgh@gmail.com>
2019-02-18 21:52:47 -0600

    define image paths in IMAGES constant

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@ import React from 'react';
 import './App.css';
 import { Image } from './Image';
 
+const IMAGES = {
+  coronation: "./imgs/coronation.jpg",
+  horseman: "./imgs/horseman.jpg",
+  intervention: "./imgs/intervention.jpg",
+  oath: "./imgs/oath.jpg",
+  recamier: "./imgs/racamier.jpg"
+}
+
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <Image />
+        <img src={IMAGES.coronation} />
       </div>
     );
   }
